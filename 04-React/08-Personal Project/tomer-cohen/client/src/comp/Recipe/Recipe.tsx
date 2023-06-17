56f846d8457d41d66beb92dc626093df9fab02f8
tomer cohen <tomer66331@gmail.com>
2023-06-17 15:35:36 +0300

    render Recipe

---
+++ 04-React/08-Personal Project/tomer-cohen/client/src/comp/Recipe/Recipe.tsx
@@ -0,0 +1,29 @@
+import { FC, SetStateAction } from "react";
+// import { useParams } from 'react-router-dom';
+import { Recipe, User } from "../../App";
+
+interface RecipeProps {
+  setRecipe: SetStateAction<any>;
+  recipes: Recipe[];
+  recipe: Recipe | undefined;
+  user:User | undefined;
+}
+// interface UserProps{
+//     setUser: SetStateAction<any>;
+//     users: User[];
+//     user: User;
+// }
+
+const recipe: FC<RecipeProps> = ({ recipe,user }) => {
+  // const { recipeId } = useParams<{ recipeId: string }>();
+
+  return (
+    <div className="recipe-container">
+        <p>{user?.userName}</p>
+      <h1>{recipe?.title}</h1>
+      <p>{recipe?.description}</p>
+    </div>
+  );
+};
+
+export default recipe;
