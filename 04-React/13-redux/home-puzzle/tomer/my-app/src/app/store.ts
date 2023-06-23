aa93797ff73f13169cdd84164771d06e302b0593
tomer cohen <tomer66331@gmail.com>
2023-06-23 22:38:54 +0300

    home work without style

---
+++ 04-React/13-redux/home-puzzle/tomer/my-app/src/app/store.ts
@@ -0,0 +1,21 @@
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
+import counterReducer from "../features/counter/counterSlice"
+import imagesReducer from "../features/image/imageSlice"
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    images: imagesReducer,
+  },
+
+})
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>
+
