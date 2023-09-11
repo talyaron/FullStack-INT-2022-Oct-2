263befbb90678abb3e081d109857d54b63da705b
itayz1e <itayz1e@gmail.com>
2023-09-11 20:59:19 +0300

    add router and navigate in pages

--- 09-React-native/02-core-components/class-puzzle/itayAmosi/itayAmosi-App/pages/Login.tsx
+++ 09-React-native/02-core-components/class-puzzle/itayAmosi/itayAmosi-App/pages/Login.tsx
@@ -4,36 +4,43 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
-  Image,
+  Button,
   ImageBackground,
 } from "react-native";
 import styles from "../style/loginStyle";
 
-const Login = () => {
-  function onPress(event: any): void {
-    console.log("test");
-  }
+
+
+const Login = ({navigation}: any) => {
+  const image = {uri: 'https://st2.depositphotos.com/3746151/5305/v/950/depositphotos_53050219-stock-illustration-dog-footsteps-pattern.jpg'};
 
   return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Login</Text>
-        <View style={styles.form}>
-          <View style={styles.inputContainer}>
-            <TextInput style={styles.input} placeholder="Username" />
-          </View>
-          <View style={styles.inputContainer}>
-            <TextInput style={styles.input} placeholder="password" />
-          </View>
-          <TouchableOpacity
-            onPress={() => {
-              console.log("test");
-            }}
-            style={styles.button}
-          >
-            <Text style={styles.buttonText}>Login</Text>
-          </TouchableOpacity>
+<ImageBackground style={styles.image} source={image}>
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <View style={styles.form}>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input} placeholder="Username" />
         </View>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input} placeholder="password" />
+        </View>
+        <TouchableOpacity
+          onPress={() => {
+            console.log("test");
+          }}
+          style={styles.button}
+          >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
       </View>
+      <Button
+        title="Go Home"
+        onPress={() => navigation.navigate('Home')}
+        />
+    </View>
+
+        </ImageBackground>
   );
 };
 
