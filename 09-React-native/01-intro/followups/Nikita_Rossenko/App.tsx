1efbeba7b04c419a112240e9da2a9c92a14568f8
Nikita Rossenko <darkcaty1999@live.ru>
2023-09-11 18:15:44 +0300

    Update App.tsx

--- 09-React-native/01-intro/followups/Nikita_Rossenko/App.tsx
+++ 09-React-native/01-intro/followups/Nikita_Rossenko/App.tsx
@@ -1,49 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput, Button, Pressable, TouchableOpacity  } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, Pressable, TouchableOpacity, SafeAreaView  } from 'react-native';
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <View style={{maxWidth:600}}>
-        <Text style={{color:"gray"}}>
-          Login Page
-        </Text>
-        <View style={styles.flexCenter}>
+    <SafeAreaView style={{flex:1}}>
+      <View style={styles.container}>
+        <View style={{maxWidth:600}}>
+          <Text style={{color:"gray"}}>
+            Login Page
+          </Text>
           <View style={styles.flexCenter}>
-            <TextInput style={styles.loginInput} placeholder="Enter your name" />
-            <TextInput style={styles.loginInput} placeholder="Enter your name" />
-          </View>
-          <View style={styles.flexCenter}>
-            <TouchableOpacity onPress={() => console.log("Pressed Login")} style={styles.loginButton}>
-              <Text>Login</Text>
-            </TouchableOpacity>
-            <Text>
-              Forgot Your password?
-            </Text>
-          </View>
-          <View style={styles.flexCenter}>
-            <Text>
-              or connect with
-            </Text>
-            <View style={styles.flexRow}>
-              <TouchableOpacity onPress={() => console.log("Pressed Facebook")} style={styles.socialLoginButton}>
-                <Text>Facebook</Text>
+            <View style={styles.flexCenter}>
+              <TextInput style={styles.loginInput} placeholder="Enter your name" />
+              <TextInput style={styles.loginInput} placeholder="Enter your name" />
+            </View>
+            <View style={styles.flexCenter}>
+              <TouchableOpacity onPress={() => console.log("Pressed Login")} style={styles.loginButton}>
+                <Text>Login</Text>
               </TouchableOpacity>
-              <TouchableOpacity onPress={() => console.log("Pressed X")} style={styles.socialLoginButton}>
-                <Text>X</Text>
-              </TouchableOpacity>
+              <Text>
+                Forgot Your password?
+              </Text>
+            </View>
+            <View style={styles.flexCenter}>
+              <Text>
+                or connect with
+              </Text>
+              <View style={styles.flexRow}>
+                <TouchableOpacity onPress={() => console.log("Pressed Facebook")} style={styles.socialLoginButton}>
+                  <Text>Facebook</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => console.log("Pressed X")} style={styles.socialLoginButton}>
+                  <Text>X</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View>
+              <Text style={{bottom:40}}>
+                Don't have an account? Sign up
+              </Text>
             </View>
           </View>
-          <View>
-            <Text style={{bottom:40}}>
-              Don't have an account? Sign up
-            </Text>
-          </View>
         </View>
-      </View>
-      {/* <StatusBar style="auto" /> */}
-    </View>
+        {/* <StatusBar style="auto" /> */}
+      </View> 
+    </SafeAreaView>
   );
 }
 
