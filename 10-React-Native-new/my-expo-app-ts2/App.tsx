1a6aed38e85a7c6ed6c6fc0d3453981786098132
Gagi Shmagi <gagi.shmagi@gmail.com>
2023-11-30 17:38:04 +0200

    Added stack navigation to the app

--- 10-React-Native-new/my-expo-app-ts2/App.tsx
+++ 10-React-Native-new/my-expo-app-ts2/App.tsx
@@ -2,12 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import TodoScreen from './screen/TodoScreen';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import About from './screen/About';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <TodoScreen/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* <Stack.Screen name="About" component={About} />
+        <Stack.Screen name="Todo App" component={TodoScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   {/* <TodoScreen/> */}
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
